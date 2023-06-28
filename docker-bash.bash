fe9ac66d684efc740d0922b07a1ed9e4d32509c0
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-06-28 10:50:35 -0600

    Config. del contenedor para postgresql y pgadmin

---
+++ docker-bash.bash
@@ -0,0 +1,14 @@
+# Levantar los servicios del contenedor
+# Accedemos a la carpeta raíz donde está el archico docker-compose.yml
+docker-compose up -d
+docker-compose up -d (service-name)
+
+# Ver los servicios levantados
+docker-compose ps
+
+# Parar un servicio
+docker-compose down
+docker-compose down (service-name)
+
+# Conectarse a la terminal del contendor
+docker-compose exec postgres bash
